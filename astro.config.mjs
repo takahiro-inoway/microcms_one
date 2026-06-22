be7cca0cb8e21fcbb83c8205a9f4d70cc5ff8248
takahiro-inoway <takahiro.inoway@email.com>
2026-06-23 01:18:12 +0900

    microcms temp setup completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "http://localhost:4321/",
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: vercel(),
 });
